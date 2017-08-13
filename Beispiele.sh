e2d31c650d7a0fe88514f544f343e718b6ecc496
Helpi <helpi9007@gmail.com>
2017-08-13 13:52:59 +0200

    add Beispiel.sh

---
+++ Beispiele.sh
@@ -0,0 +1,26 @@
+# Beispiele
+#
+#######################################
+##	Host und Eingabe prüfen	     ##
+#######################################
+#Pruefen, ob Suffix angegeben
+if [ -z "$1" ] ;then
+    echo "So geht das nicht"
+    exit 1;
+fi
+
+#Prüfen, ob Host erreichbar
+ping -c 1 192.168.2.$1 &> /dev/null
+if [ "$?" != 0 ] ; then
+        echo "Offline"
+        exit 1;
+fi
+#######################################
+##	Uptime	     ##
+#######################################
+# Verbindung mittels ssh herstellen und Laufzeit abfragen
+up=$(/usr/bin/ssh -i /scripts/.ssh/id_rsa root@192.168.2.$1 "uptime -p")
+
+
+# Ausgabe von Uptime in Deutsch umwandeln.
+echo $up | sed 's/up //g' | sed 's/, / /g' | sed 's/days/Tage/g' | sed 's/day/Tag/g' | sed 's/hours/Stunden/g' | sed 's/hour/Stunde/g' | sed 's/minutes/Minuten/g' | sed 's/minute/Minute/g'
